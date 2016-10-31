c23defa45efcc0ac52563ded74c130c98b1b8f03
zhangyunfeng <zhangyunfeng0101@gmail.com>
2016-11-01 00:14:15 +0800

    commit files

--- src/AtoI.hpp
+++ src/AtoI.hpp
@@ -1,42 +1,54 @@
 #ifndef ATOI_H
 #define ATOI_H
 
-#include <string>
+#include <algorithm>
 #include <exception>
-#include <limits.h>
 #include <iostream>
+#include <limits.h>
+#include <string>
 
 using namespace std;
 
-int atoi(const string &str) {
+int myAtoi(string &str) {
   int len = str.length();
   int s = 0;
-  int e = len-1;
-  while(s < len && isspace(str.at(s))) {
+  int e = len - 1;
+  while (s < len && isspace(str.at(s))) {
     s++;
   }
-  while(e >= 0 && isspace(str.at(e))) {
+  while (e >= 0 && isspace(str.at(e))) {
     e--;
   }
   if (e == -1) {
     return 0;
   }
-  string str2 = str.substr(s, e-s+1);
+
+  string str2 = str.substr(s, e - s + 1);
+
   len = str2.length();
   long long result = 0L;
-  for(int i = len-1; i >= 0; i--) {
+  int sign = 1;
+  int index = 0;
+  if (str2.at(0) == '-') {
+    sign = -1;
+    index = 1;
+  }
+  if (str2.at(0) == '+') {
+    index = 1;
+  }
+  for (int i = index; i < len; i++) {
     char c = str2.at(i);
     if (c >= '0' && c <= '9') {
-      result = result * 10 + (long long)(c-32);
-    }
-    if (i == 0) {
-      if (c == '-') {
-        result *= -1L;
-      }
+      result = result * 10L + (c - '0');
+      if (result*sign <= (long long)INT_MIN) // NOTE: here is result(*sign not only result)
+        return INT_MIN;
+      if (result*sign >= (long long)INT_MAX)
+        return INT_MAX;
+    } else {
+      break;
     }
   }
-  if (result < INT_MIN) return INT_MIN;
-  if (result > INT_MAX) return INT_MAX;
+  result *= sign;
   return int(result);
 }
 
--- test/AtoITest.cpp
+++ test/AtoITest.cpp
@@ -4,18 +4,36 @@
 #include "../src/AtoI.hpp"
 #include "external/single_include/catch.hpp"
 
+TEST_CASE("AtoITest", "[myAtoi]") {
+  string s("123");
+  REQUIRE(myAtoi(s) == 123);
+  string s1("1");
+  REQUIRE(myAtoi(s1) == 1);
+  string s_1("-1");
+  REQUIRE(myAtoi(s_1) == -1);
+  string s2("+1");
+  REQUIRE(myAtoi(s2) == 1);
+  string s3("-123");
+  REQUIRE(myAtoi(s3) == -123);
+  string s4("0");
+  REQUIRE(myAtoi(s4) == 0);
+  string s5("-00");
+  REQUIRE(myAtoi(s5) == 0);
+  string s6("00001");
+  REQUIRE(myAtoi(s6) == 1);
 
-TEST_CASE("AtoITest", "[atoi]") {
-  REQUIRE(atoi("123") == 123);
-  REQUIRE(atoi("1") == 1);
-  REQUIRE(atoi("-1") == -1);
-  REQUIRE(atoi("+1") == 1);
-  REQUIRE(atoi("-123") == -123);
-  REQUIRE(atoi("0") == 0);
-  REQUIRE(atoi("-00") == 0);
-  REQUIRE(atoi("00001") == 1);
-  REQUIRE(atoi("  123  ") == 123);
-  REQUIRE(atoi("99999999999") > INT_MAX);
+  string s7("   123");
+  REQUIRE(myAtoi(s7) == 123);
+  string s8("2147483648");
+  REQUIRE(myAtoi(s8) == 2147483647);
+  string s9("+-2");
+  REQUIRE(myAtoi(s9) == 0);
+  string s10("   -0012a42");
+  REQUIRE(myAtoi(s10) == -12);
+  string s11("9223372036854775809");
+  REQUIRE(myAtoi(s11) == 2147483647);
+  string s12("-2147483648");
+  REQUIRE(myAtoi(s12) == -2147483648);
 }
 
-#endif /* ATOITEST_H */
+#endif /* MYATOITEST_H */
