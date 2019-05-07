c6130d272e9fe6b03953133206595d48832ed3b4
zayfen <zhangyunfeng0101@gmail.com>
2019-05-07 22:41:44 +0800

    chore: access four_sum

--- rust/leetcode/src/hash_table/four_sum.rs
+++ rust/leetcode/src/hash_table/four_sum.rs
@@ -36,19 +36,32 @@ pub fn four_sum(nums: Vec<i32>, target: i32) -> Vec<Vec<i32>> {
   store
 }
 
+
+
 // assume nums is sorted
 pub fn n_sum (nums: &[i32], target: i32, n: i32, mut result: Vec<i32>, mut store: &mut Vec<Vec<i32>>) {
-  
+  println!("n: {:?} ; result: {:?}", n, result);
   if n == 0 {                   // print result
+  
     if target == 0 {
       println!("{:?}", result);
 
       if store.len() > 0 {
-        let last_result = &store[store.len() - 1];
-        if !(last_result.iter().zip(&result).all(|(a, b)| *a == *b)) {
+        let mut existed = false;
+        for item in store.iter() {
+          if item.iter().zip(&result).all(|(a, b)| *a == *b) {
+            existed = true;
+          }
+        }
+        
+        if !existed {
           store.push(result);
         }
-
+        // let last_result = &store[store.len() - 1];
+        // if !(last_result.iter().zip(&result).all(|(a, b)| *a == *b)) {
+        //   store.push(result);
+        // }
+        
       } else {
         store.push(result);
       }
@@ -64,11 +77,11 @@ pub fn n_sum (nums: &[i32], target: i32, n: i32, mut result: Vec<i32>, mut store
     let number = nums[i];
 
     // if target-number >= number, go on
-    if target >= number {
-      let next_nums = &nums[i+1..];
-      let next_target = target - number;
+    // if target >= number {
+    let next_nums = &nums[i+1..];
+    let next_target = target - number;
+    if next_target.abs() <= target.abs() {
       let mut next_result = result.clone();
-
       next_result.push(number);
       n_sum(next_nums, next_target, n-1, next_result, store);
     }
@@ -77,10 +90,58 @@ pub fn n_sum (nums: &[i32], target: i32, n: i32, mut result: Vec<i32>, mut store
 }
 
 
+pub fn n_sum_v2 (nums: &[i32], target: i32, n: i32, mut result: Vec<i32>, mut store: &mut Vec<Vec<i32>>) {
+  let len_nums = nums.len();
+  // early return
+  if len_nums < 2 || n < 2 || nums[0] * n > target || target > nums.last().unwrap() * n {
+    return ()
+  }
+  
+  if n == 2 {
+    let mut left = 0;
+    let mut right = len_nums - 1;
+    while left < right {
+      let sum = nums[left] + nums[right];
+      let mut result = result.clone();
+      println!("nums: {:?}", nums);
+      println!("n_sum_v2 left: {:?} ;right: {:?} ;sum: {:?} ;target: {:?}", left, right, sum, target);
+      if sum == target {
+        result.push(nums[left]);
+        result.push(nums[right]);
+        store.push(result);
+        left += 1;
+        while left < right && nums[left] == nums[left-1] {
+          left += 1;
+        }
+      } else if sum < target {
+        left += 1;
+      } else {
+        right -= 1;
+      }
+    }
+  } else {
+    let i32_len_nums = len_nums as i32;
+    let range_index = i32_len_nums - n;
+    let mut i = 0;
+    while i <= range_index {
+      let i32_i = i as usize;
+      if i == 0 || (i > 0 && nums[i32_i-1] != nums[i32_i]) {
+        let number = nums[i32_i];
+        let mut result = result.clone();
+        result.push(number);
+        n_sum_v2(&nums[(i32_i+1)..len_nums], target-number, n-1, result, store);
+      }
+      
+      i+=1;
+    }
+  }
+}
+
+
 #[cfg(test)]
 mod hash_table_tests {
   use super::*;
-
+  
   #[test]
   fn test_four_sum () {
     let mut result: Vec<i32> = Vec::new();
--- rust/leetcode/src/main.rs
+++ rust/leetcode/src/main.rs
@@ -9,12 +9,29 @@ fn main() {
   let mut result: Vec<i32> = Vec::new();
   // let nums = &[-2, -1, 0, 0, 1, 2]; //
   // let nums = &[1, 0, -1, 0, -2, 2];
-  let nums = &[-3, -2, -1, 0, 0, 1, 2, 3];
+  // let nums = &[-3, -2, -1, 0, 0, 1, 2, 3];
+  let nums = &[0, 0];
   let mut nums = nums.clone();
   nums.sort();
   let nums  = nums.clone();
   let mut store: Vec<Vec<i32>> = Vec::new();
-  
-  hash_table::four_sum::n_sum(&nums, 0, 4, result, &mut store);
+
+  println!(">>>>>>>>>>>>>>>>>> 0000000 >>>>>>>>>");
+  println!("sorted numbers: {:?}", nums);
+  hash_table::four_sum::n_sum_v2(&nums, 0, 4, result, &mut store);
   println!("store: {:?} ", store);
+
+  println!(">>>>>>>>>>>>>>>>>>> 11111111 >>>>>>>>>>>>>>>>>>>");
+
+  let nums = &[-494,-487,-471,-470,-465,-462,-447,-445,-441,-432,-429,-422,-406,-398,-397,-364,-344,-333,-328,-307,-302,-293,-291,-279,-269,-269,-268,-254,-198,-181,-134,-127,-115,-112,-96,-94,-89,-58,-58,-58,-44,-2,-1,43,89,92,100,101,106,106,110,116,143,156,168,173,192,231,248,256,281,316,321,327,346,352,353,355,358,365,371,410,413,414,447,473,473,475,476,481,491,498];
+  let mut nums = nums.clone();
+  nums.sort();
+  let nums  = nums.clone();
+  let mut store: Vec<Vec<i32>> = Vec::new();
+  let mut result: Vec<i32> = Vec::new();
+  
+  hash_table::four_sum::n_sum_v2(&nums, 8511, 4, result, &mut store);
+  println!("store: {:?} ", store);
+
+  println!("<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<");
 }
