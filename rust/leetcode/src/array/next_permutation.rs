3af64bab440b558f565407bb69cc2903ef054a27
zayfen <zhangyunfeng0101@gmail.com>
2019-05-20 22:42:50 +0800

    chore: add sudoku

--- rust/leetcode/src/array/next_permutation.rs
+++ rust/leetcode/src/array/next_permutation.rs
@@ -1,25 +1,16 @@
-/**
-Implement next permutation, which rearranges numbers into the lexicographically next greater permutation of numbers.
+/// 31. Next Permutation
+/// https://leetcode.com/problems/next-permutation/
 
-If such arrangement is not possible, it must rearrange it as the lowest possible order (ie, sorted in ascending order).
-
-The replacement must be in-place and use only constant extra memory.
-
-Here are some examples. Inputs are in the left-hand column and its corresponding outputs are in the right-hand column.
-
-1,2,3 → 1,3,2
-3,2,1 → 1,2,3
-1,1,5 → 1,5,1
-
- **/
-
+struct Solution ();
 
 impl Solution {
+
   pub fn next_permutation(nums: &mut Vec<i32>) {
+    println!("{:?}", nums);
+
     let nums_len = nums.len();
 
     let (mut left, mut right) = (0, 0);
-
     let mut i = nums_len - 1;
     while i >= 1 {
       if nums[i-1] < nums[i] {
@@ -27,39 +18,71 @@ impl Solution {
         right = i;
         break;
       }
-      i-=1;
+      i -= 1;
     }
-
 
     if left ==  0 && right == 0 {
       nums.reverse();
-    } else {
-      // find min({k | nums[k] > nums[left], 且k >= right })
-      let mut k = right;
-      let left_num = nums[left];
-      for i in right..nums_len {
-        if nums[i] > left_num && nums[i] <= nums[k] {
-          k = i
-        }
-      }
-
-      nums[left] = nums[k];
-      nums[k] = left_num;
-
-      // reverse nums[k+1..]
-      let mut step = 1;
-      for i in right..nums_len {
-        let (l, r) = (i, nums_len - step);
-        if l >= r {
-          return;
-        }
-
-        let tmp = nums[l];
-        nums[l] = nums[r];
-        nums[r] = tmp;
-        step += 1;
-      }
-
+      return;
     }
+
+    // find min({k | nums[k] > nums[left] 且 k >= right })
+    let mut k = right;
+    let left_num = nums[left];
+    for i in right..nums_len {
+      if nums[i] > left_num && nums[i] <= nums[k] {
+        k = i
+      }
+    }
+
+    nums[left] = nums[k];
+    nums[k] = left_num;
+
+    // reverse nums[k+1..]
+    println!("left: {:?}; right: {:?}", left, right);
+
+    let mut step = 1;
+    for i in right..nums_len {
+      let (l, r) = (i, nums_len - step);
+      if l >= r {
+        return;
+      }
+
+      let tmp = nums[l];
+      nums[l] = nums[r];
+      nums[r] = tmp;
+      step += 1;
+    }
+
+  }
+}
+
+
+#[cfg(test)]
+mod array_test {
+  use super::*;
+
+  #[test]
+  fn next_permutation_test () {
+    let mut vec = vec!(1, 2, 3);
+    Solution::next_permutation(&mut vec);
+    assert_eq!(vec!(1, 3, 2), vec);
+
+    let mut vec2 = vec!(3, 2, 1);
+    Solution::next_permutation(&mut vec2);
+    assert_eq!(vec!(1, 2, 3), vec2);
+
+    let mut vec3 = vec!(1, 1, 5);
+    Solution::next_permutation(&mut vec3);
+    assert_eq!(vec!(1, 5, 1), vec3);
+
+    let mut vec4 = vec!(1, 3, 2);
+    Solution::next_permutation(&mut vec4);
+    assert_eq!(vec!(2, 1, 3), vec4);
+
+    let mut vec5 = vec!(2, 3, 1, 3, 3);
+    Solution::next_permutation(&mut vec5);
+    assert_eq!(vec!(2, 3, 3, 1, 3), vec5);
+
   }
 }
