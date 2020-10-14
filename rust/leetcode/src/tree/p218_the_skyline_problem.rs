740ee5ed392114f97f9ac7f7db9f49a4689620df
zayfen <zhangyunfeng0101@gmail.com>
2020-10-14 22:04:11 +0800

    commit code

--- rust/leetcode/src/tree/p218_the_skyline_problem.rs
+++ rust/leetcode/src/tree/p218_the_skyline_problem.rs
@@ -1,13 +1,14 @@
-
-
 #[path = "../solution.rs"]
 mod solution;
 
 use solution::Solution;
 
+type TreeNode = (i32, i32);
+
 #[derive(Debug)]
 struct MaxSegmentTree {
-  tree: Vec<(i32, i32, i32)>
+  buildings: Vec<Vec<i32>>,
+  st_buildings: Option<Vec<TreeNode>>
 }
 
 pub(crate) fn left_child (i: usize) -> usize {
@@ -22,47 +23,52 @@ pub(crate) fn parent (i: usize) -> usize {
   i >> 1
 }
 
-type Dimensions = (i32, i32, i32);
-
 impl MaxSegmentTree {
 
-  fn new (building_dimensions: Vec<Vec<i32>>) -> Self {
-    let len = building_dimensions.len();
-    let normalized_building_dimensions: Vec<Dimensions> = building_dimensions.iter().map(|item| {
-      (item[0], item[1], item[2])
-    }).collect();
-
-    let mut tree_arr: Vec<Dimensions> = [vec![(0,0,0); len], normalized_building_dimensions].concat();
-
-    // build segment tree, max segment tree
-    let mut index = len - 1;
-    while index > 0 {
-      let left_child_index = left_child(index);
-      let right_child_index = right_child(index);
-
-      let left_child_height = tree_arr[left_child_index].2;
-      let right_child_height = tree_arr[right_child_index].2;
-      
-      tree_arr[index] = match left_child_height >= right_child_height {
-        true => tree_arr[left_child_index],
-        false => tree_arr[right_child_index]
-      };
-      
-      index -= 1;
-    }
-    
-    dbg!(&tree_arr);
+  fn new (buildings: Vec<Vec<i32>>) -> Self {
     MaxSegmentTree {
-      tree: tree_arr
+      buildings,
+      st_buildings: None
     }
   }
 
+  /// # 查询区间
+  /// @param {{ i32 }} left 左边界 闭区间
+  /// @param {{ i32 }} right 右边界 开区间
+  /// @returns {{ Option<Vec<(i32, i32)>> }}
   fn query (left: i32, right: i32) -> Option<Vec<(i32, i32)>> {
     if left <= right {
       return None;
     }
     
     None
+  }
+
+  fn build (&mut self) {
+    let mut flattened_buildings: Vec<TreeNode> = self.buildings.iter().map(|item| {
+      vec![(item[0], item[2]), (item[1], item[2])]
+    }).flatten().collect::<Vec<TreeNode>>();
+
+    let len = flattened_buildings.len();
+    let mut st_tree: Vec<TreeNode> = [vec![(0, 0); len], flattened_buildings].concat();
+
+    let mut index = len - 1;
+    while index > 0 {
+      let left_child_index = left_child(index);
+      let right_child_index = right_child(index);
+
+      let left_child_height = st_tree[left_child_index].1;
+      let right_child_height = st_tree[right_child_index].1;
+
+      st_tree[index] = match left_child_height >= right_child_height {
+        true => st_tree[left_child_index],
+        false => st_tree[right_child_index]
+      };
+
+      index -= 1;
+    }
+    dbg!(st_tree.clone());
+    self.st_buildings = Some(st_tree);
   }
 }
 
@@ -83,7 +89,8 @@ mod get_skyline_test {
   #[test]
   fn test_get_skyline () {
     let building_dimens = vec![ vec![2,9,10], vec![3,7,15], vec![5,12,12], vec![15,20,10], vec![19,24,8]];
-    let st = MaxSegmentTree::new(building_dimens);
+    let mut st = MaxSegmentTree::new(building_dimens);
+    st.build();
     dbg!(&st);
   }
 }
